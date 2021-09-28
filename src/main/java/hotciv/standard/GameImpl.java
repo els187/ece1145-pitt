3f51afbebe35ab56a44fa63ae43ae6600b63a31f
shreyakhanal6 <khanalshreya4@gmail.com>
2021-09-28 17:18:18 -0400

    Changed implementation to use hash maps instead of arrays

--- src/main/java/hotciv/standard/GameImpl.java
+++ src/main/java/hotciv/standard/GameImpl.java
@@ -1,80 +1,82 @@
 package hotciv.standard;
-//Oh no! There is a fake bug in this file. We must fix it by implementing a hotfix.
-//Fixed bug.
+
 import hotciv.framework.*;
+import java.util.*;
 
 /** Skeleton implementation of HotCiv.
- 
-   This source code is from the book 
-     "Flexible, Reliable Software:
-       Using Patterns and Agile Development"
-     published 2010 by CRC Press.
-   Author: 
-     Henrik B Christensen 
-     Department of Computer Science
-     Aarhus University
-   
-   Please visit http://www.baerbak.com/ for further information.
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
- 
-       http://www.apache.org/licenses/LICENSE-2.0
- 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+ This source code is from the book
+ "Flexible, Reliable Software:
+ Using Patterns and Agile Development"
+ published 2010 by CRC Press.
+ Author:
+ Henrik B Christensen
+ Department of Computer Science
+ Aarhus University
 
-*/
+ Please visit http://www.baerbak.com/ for further information.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+ */
 
 public class GameImpl implements Game {
   private Player playerInTurn = Player.RED;
   private int age = -4000;
 
-  private Tile[][] tiles = new Tile[GameConstants.WORLDSIZE][GameConstants.WORLDSIZE];
-  private Unit[][] units = new Unit[GameConstants.WORLDSIZE][GameConstants.WORLDSIZE];
-  private City[][] cities = new City[GameConstants.WORLDSIZE][GameConstants.WORLDSIZE];
- 
-  public GameImpl() {
+  private HashMap<Position, City> cities  = new HashMap<Position, City>();
+  private HashMap<Position, Unit> units = new HashMap<Position, Unit>();
+  private HashMap<Position, Tile> tiles = new HashMap<Position, Tile>();
+
+  public GameImpl(){
+
     for(int i = 0; i < GameConstants.WORLDSIZE;i++){
       for(int j = 0; j < GameConstants.WORLDSIZE;j++){
-        tiles[i][j] = new TileImpl(GameConstants.PLAINS);
+        tiles.put(new Position(i, j), new TileImpl(GameConstants.PLAINS));
       }
     }
 
     //Set tiles to be of types oceans, hill, mountains
-    tiles[1][0] = new TileImpl(GameConstants.OCEANS);
-    tiles[0][1] = new TileImpl(GameConstants.HILLS);
-    tiles[2][2] = new TileImpl(GameConstants.MOUNTAINS);
+    tiles.put(new Position(1, 0), new TileImpl(GameConstants.OCEANS));
+    tiles.put(new Position(0, 1), new TileImpl(GameConstants.HILLS));
+    tiles.put(new Position(2, 2), new TileImpl(GameConstants.MOUNTAINS));
 
     //Set units to be of red archers, blue legions, red settlers
-    units[2][0] = new UnitImpl(GameConstants.ARCHER, Player.RED);
-    units[3][2] = new UnitImpl(GameConstants.LEGION, Player.BLUE);
-    units[4][3] = new UnitImpl(GameConstants.SETTLER, Player.RED);
+    units.put(new Position(2,0), new UnitImpl(GameConstants.ARCHER, Player.RED));
+    units.put(new Position(3,2), new UnitImpl(GameConstants.LEGION, Player.BLUE));;
+    units.put(new Position(4,3), new UnitImpl(GameConstants.SETTLER, Player.RED));
 
-    //Set city to be owned by red and blue
-    cities[1][1] = new CityImpl(Player.RED);
-    cities[4][1] = new CityImpl(Player.BLUE);
+
+    //Set cities to be owned by red and blue
+    cities.put(new Position(1,1), new CityImpl(Player.RED));
+    cities.put(new Position(4,1), new CityImpl(Player.BLUE));
+
   }
 
   public Tile getTileAt( Position p ) {
-    return tiles[p.getRow()][p.getColumn()];
+    return tiles.get(p);
   }
-
   public Unit getUnitAt( Position p ) {
-    return units[p.getRow()][p.getColumn()];
+    return units.get(p);
   }
-
   public City getCityAt( Position p ) {
-    return cities[p.getRow()][p.getColumn()];
+    return cities.get(p);
   }
 
   public Player getPlayerInTurn() {
     return playerInTurn;
   }
+
   public Player getWinner() {
     //If age is 3000, red wins
     if(getAge() == -3000){
@@ -82,10 +84,13 @@ public class GameImpl implements Game {
     }
     return null;
   }
-  public int getAge() { return age; }
+
+  public int getAge() {
+    return age;
+  }
+
   public boolean moveUnit( Position from, Position to ) {
     Unit unitFrom = getUnitAt(from);
-    Unit unitTo = getUnitAt(to);
     Tile tileTo = getTileAt(to);
 
     //Check if the destinationTile contains Mountains or Oceans
@@ -93,32 +98,33 @@ public class GameImpl implements Game {
       return false;
     }
 
+    //Check if the player is trying to move another player's unit
     if (unitFrom.getOwner() != playerInTurn)
       return false;
 
     //If destination tile is less than 1 unit from sourceTile, then move it
     if (Math.abs(to.getRow() - from.getRow()) <= 1 && Math.abs(to.getColumn() - from.getColumn()) <= 1) {
-      //If tile isn't occupied then move the unit, set the old tile equal to null
+      //If destination tile isn't occupied then move the unit and remove unit from old position
       if(getUnitAt(to) == null) {
-        units[to.getRow()][to.getColumn()] = unitFrom;
-        units[from.getRow()][from.getColumn()] = null;
+        units.put(to, unitFrom);
+        units.remove(from);
         return true;
       } //at this point, we have checked if the tile is occupied, so we can check if attack
       if(attackOnEnemySucceeded(from, to)) {return true;}
     }
     return false;
   }
+
   //In Alpha Civ, the attacker is always successful.
   private boolean attackOnEnemySucceeded(Position from, Position to) {
-      boolean successfulAttack = true;
-      if (successfulAttack) {
-        units[to.getRow()][to.getColumn()] = getUnitAt(from); //moves unit to tile
-        units[from.getRow()][from.getColumn()] = null; //deletes old position
-        return true;
-      }
+    boolean successfulAttack = true;
+    if (successfulAttack) {
+      units.put(to, getUnitAt(from));
+      units.remove(from);
+      return true;
+    }
     return false;
   }
-
 
   public void endOfTurn() {
     //If current player is red, switch to blue and add 6 productions
@@ -140,7 +146,8 @@ public class GameImpl implements Game {
   public void changeWorkForceFocusInCityAt( Position p, String balance ) {}
 
   public void changeProductionInCityAt( Position p, String unitType ) {
-    CityImpl city = (CityImpl) cities[p.getRow()][p.getColumn()];
+    CityImpl city = (CityImpl) cities.get(p);
+    city.setProduction(unitType);
     produceUnit(city,p,unitType);
   }
 
@@ -156,23 +163,25 @@ public class GameImpl implements Game {
     }
   }
 
+  //*********************************************METHODS THAT WE ADDED**************************************************
+
   public void produceUnit(CityImpl c,  Position p, String unitType ){
     //If unitType is Archer and has enough to buy an archer, produce an Archer
     if (unitType.equals(GameConstants.ARCHER) && c.getTreasury() >= 10) {
       c.setProduction(GameConstants.ARCHER);
-    //If unitType is Legion and has enough to buy a legion, produce a Legion
+      //If unitType is Legion and has enough to buy a legion, produce a Legion
     } else if (unitType.equals(GameConstants.LEGION) && c.getTreasury() >= 15) {
       c.setProduction(GameConstants.LEGION);
-    //If unitType is Settler and has enough to buy a settler, produce a Settler
+      //If unitType is Settler and has enough to buy a settler, produce a Settler
     } else if (unitType.equals(GameConstants.SETTLER) && c.getTreasury() >= 30) {
       c.setProduction(GameConstants.SETTLER);
     }
 
     //Deduct the unitType's cost from the treasury
-    c.removeTreasury(unitCost(c.getProduction()));
+    c.removeTreasury(getUnitCost(c.getProduction()));
   }
 
-  private int unitCost(String type) {
+  private int getUnitCost(String type) {
     if (type.equals(GameConstants.ARCHER)) {
       return 10;
     }
