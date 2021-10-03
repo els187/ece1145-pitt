c6eba8e140ca80dd9331e3026abeac0a6f9eed71
Lizzie S <els187@pitt.edu>
2021-10-03 00:10:06 -0400

    Implemented Map Strategy from stubgame1.java

--- src/main/java/hotciv/framework/MapStrategy.java
+++ src/main/java/hotciv/framework/MapStrategy.java
@@ -1,4 +1,7 @@
 package hotciv.framework;
 
+import java.util.Map;
+
 public interface MapStrategy {
+    public default Map<Position, Tile> defineWorld() {return null;}
 }
--- src/main/java/hotciv/standard/DeltaCityStart.java
+++ src/main/java/hotciv/standard/DeltaCityStart.java
@@ -1,4 +1,4 @@
 package hotciv.standard;
-
+import hotciv.framework.*;
 public class DeltaCityStart implements CityStart {
 }
