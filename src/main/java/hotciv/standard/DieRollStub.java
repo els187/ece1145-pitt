d554098f7d6f133b8c63f5cf8ef3e59432adfe46
shreyakhanal6 <khanalshreya4@gmail.com>
2021-10-31 21:16:21 -0400

    Added a private field to DieRollStub

--- src/main/java/hotciv/standard/DieRollStub.java
+++ src/main/java/hotciv/standard/DieRollStub.java
@@ -3,9 +3,10 @@ package hotciv.standard;
 import hotciv.framework.DieRollStrategy;
 
 public class DieRollStub implements DieRollStrategy {
+    private int fixedRoll = 1;
 
     @Override
     public int getDieRoll(){
-        return 1;
+        return fixedRoll;
     }
 }
